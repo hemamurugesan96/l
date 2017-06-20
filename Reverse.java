60203a3a62f02330270d459d28f6e2156849a16b
hemamurugesan96 <hemamurugesanece@gmail.com>
2017-06-20 16:47:04 +0530

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+class Reverse
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String s=str.reverse();
+System.out.println(s);
+}
+}
